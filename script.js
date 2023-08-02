e8713d2050cbed02ff4f9bf43e64f4bbf484bc10
hiteshpiplani <104992875+hiteshpiplani@users.noreply.github.com>
2023-08-02 13:51:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 function convertTemperature() {
-    const temperatureInput = document.getElementById('temperatureInput').value;
-    const unitSelect = document.getElementById('unitSelect').value;
-    let convertedTemperature;
-  
-    if (unitSelect === 'celsius') {
-      convertedTemperature = celsiusToFahrenheit(temperatureInput);
-    } else if (unitSelect === 'fahrenheit') {
-      convertedTemperature = fahrenheitToCelsius(temperatureInput);
-    }
-  
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = `Converted temperature: ${convertedTemperature.toFixed(2)} ${unitSelect.toUpperCase()}`;
+  const temperatureInput = document.getElementById('temperatureInput').value;
+  const unitSelect = document.getElementById('unitSelect').value;
+  let convertedTemperature;
+
+  if (unitSelect === 'celsius') {
+    convertedTemperature = celsiusToFahrenheit(temperatureInput);
+  } else if (unitSelect === 'fahrenheit') {
+    convertedTemperature = fahrenheitToCelsius(temperatureInput);
   }
-  
-  function celsiusToFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
-  }
-  
-  function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
-  }
-  
+
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = `Converted temperature: ${convertedTemperature.toFixed(2)} ${unitSelect === 'celsius' ? 'Fahrenheit' : 'Celsius'}`;
+}
+
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
